6b8c38f76ef883a4ca093fe9b39271903d6fc7af
alaa-alnissany <alaa.alnissany@gmail.com>
2025-10-15 23:00:29 +0800

    --add Data Generator

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 from mlflow import MlflowClient
-from pprint import pprint
-from sklearn.ensemble import RandomForestClassifier
+import mlflow
 import dotenv
 import os
+import training_code
+import pandas as pd
+from generate_data import generate_apple_sales_data_with_promo_adjustment
 
 dotenv.load_dotenv()
-client = MlflowClient(tracking_uri= os.getenv('MLFLOW_TRACKING_URI'))
-all_experiments = client.search_experiments()
-default_experiment = [
-                    {"name":experiments.name,"lifecycle_stage":experiments.lifecycle_stage} 
-                    for  experiments in all_experiments
-                    if experiments.name == "Default"
-                    ][0]
-print(default_experiment)
+
+### USING MLFLOW CLIENT
+# client = MlflowClient(tracking_uri= os.getenv('MLFLOW_TRACKING_URI'))
+# all_experiments = client.search_experiments()
+# default_experiment = [
+#                     {"name":experiments.name,"lifecycle_stage":experiments.lifecycle_stage} 
+#                     for  experiments in all_experiments
+#                    if experiments.name == "Default"
+#                    ][0]
+# print(default_experiment)
+
+
+### USING MLFLOW FLUENT API
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+apple_experiment=mlflow.set_experiment(os.getenv("MLFLOW_EXPERIMENT_NAME")) 
+RUN_NAME = os.getenv("MLFLOW_RUN_NAME")
+ARTIFACT_PATH = os.getenv("MLFLOW_ARTIFACT_PATH")
+data_path = "./data/apple_sales_data.csv"
+
+
+try:
+    df = pd.read_csv(data_path)
+except Exception as e:
+    print(f"{e}\nGenerating Data ...")
+    df = generate_apple_sales_data_with_promo_adjustment( base_demand= 1000, n_rows = 5000)
+    df.to_csv(data_path)
+    print("The Data was generated successfully")
+
---
+++ training_code.py
@@ -0,0 +1,4 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
