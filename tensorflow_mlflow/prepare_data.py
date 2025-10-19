d890754af3bcdb6f8daeefb98ead4da358df133d
alaa-alnissany <alaa.alnissany@gmail.com>
2025-10-20 00:27:33 +0800

    --clean prepare_data.py

--- tensorflow_mlflow/prepare_data.py
+++ tensorflow_mlflow/prepare_data.py
@@ -1,29 +1,15 @@
 # Important Libs for clean coding
-from typing import Dict, Tuple, Any, Optional
+from typing import Dict, Tuple
 import logging
-import warnings
 
 # Basic libs
 import pandas as pd 
 import numpy as np
-import matplotlib.pyplot as plt
 
 # Machine learning Libs
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 
-# Deep Learning Libs
-from sqlalchemy import true
-import tensorflow as tf
-from tensorflow import keras
-
-# Additional Deep Learning Libs 
-from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
-
-# MLOPS Libs
-import mlflow
-from mlflow.models import infer_signature
 
 logger = logging.getLogger("__main__")
 
